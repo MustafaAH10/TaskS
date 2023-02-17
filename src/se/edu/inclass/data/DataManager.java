9cd76dd160467beddfe7206463f51631af7a5936
Mustafa Anis Hussain <mustafahussain793@gmail.com>
2023-02-17 16:48:11 +0800

    Update error handling

--- src/se/edu/inclass/data/DataManager.java
+++ src/se/edu/inclass/data/DataManager.java
@@ -57,7 +57,8 @@ public class DataManager {
             ArrayList<String> dataItems = readFile();
             taskList = parse(dataItems);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("File access issues. Please Check");
+            //e.printStackTrace(); bad way of exception handling
         }
         return taskList;
     }
